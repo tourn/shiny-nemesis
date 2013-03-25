1d8433e9600fc644e9a2d5cee725893467f505ae
Tourniquet <latzer.daniel@gmail.com>
2013-03-25 19:05:51 +0100

    Refactor CountdownActivity
    removed unused MainActivity

--- Shiny Nemesis/src/ch/bulletproof/countdown/CountdownActivity.java
+++ Shiny Nemesis/src/ch/bulletproof/countdown/CountdownActivity.java
@@ -15,72 +15,62 @@ import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.TextView;
 
-public class CountdownActivity extends Activity implements OnTouchListener{
-	public static final String MINUTES = "tasty pie";
+/**
+ * @author tourn
+ * 
+ * Starts a countdown ending in the next given MINUTE of the hour, the minute being passed
+ * over an intent
+ *
+ */
+public class CountdownActivity extends Activity{
+	public static final String MINUTES = "minutes";
 	
-	Player player;
-	boolean loaded = false;
-	Timer timer;
-	long endTime;
+	private Player player;
+	private Timer timer;
   
-	/** Called when the activity is first created. */
-
 	@Override
   	public void onCreate(Bundle savedInstanceState) {
-		setContentView(R.layout.activity_countdown);
-		View view = findViewById(R.id.btn_cancel);
-		view.setOnTouchListener(this);
+		super.onCreate(savedInstanceState);
 		
+		//check if the intent gave us a valid minute
 		Intent intent = getIntent();
 		int minutes = intent.getIntExtra(CountdownActivity.MINUTES, -1);
 		
 		if(minutes > 59 || minutes < 0)
 			throw new IllegalArgumentException();
 		
-		Calendar endCalendar = Calendar.getInstance();
-		if(endCalendar.get(Calendar.MINUTE) >= minutes)
-			endCalendar.add(Calendar.HOUR, 1);
-		endCalendar.set(Calendar.SECOND, 0);
-		endCalendar.set(Calendar.MINUTE, minutes);
+		//set up the view
+		setContentView(R.layout.activity_countdown);
 		
-		timer = new Timer();
-		player = new Player(Setup.BASE_VOICESET_DIR,Setup.DEFAULT_VOICESET_DIR);
-		
-		endTime = endCalendar.getTimeInMillis(); 
-		super.onCreate(savedInstanceState);
+		View vbCancel = findViewById(R.id.btn_cancel);
+		vbCancel.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				timer.cancel();
+				finish();
+				return true;
+			}
+		});
 		
 		// Set the hardware buttons to control the music
 		this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
 		
-		//Schedule sounds
-		for (Sound sound : player.getSounds()) {
-//			long schedule = endTime - 1000 * sound.getSecToEnd();
-			long schedule = endTime - 1000 * sound.getSecToEnd() - 1000;
-			if(schedule >= Calendar.getInstance().getTimeInMillis())
-				timer.schedule(new SoundTask(sound), new Date(schedule));
-		}
 		
-		timer.schedule(new TimerTask(){
-			@Override
-			public void run() {
-				finish();
-			}
-		}, new Date(endTime));
+		// make a calendar pointing to the given minute in this hour
+		long endTime = getNextMinuteOccurrence(minutes).getTimeInMillis();
 		
-//		timer.schedule(new TimerTask(){
-//
-//			@Override
-//			public void run() {
-//				TextView text = (TextView) findViewById(R.id.countdown);
-//				long now = Calendar.getInstance().getTimeInMillis();
-//				
-//				Calendar remainingTime = Calendar.getInstance();
-//				remainingTime.setTimeInMillis(endTime - now);
-//				
-//				text.setText(remainingTime.get(Calendar.MINUTE) + ":" + remainingTime.get(Calendar.SECOND));
-//			}
-//		}, 0, 1000);
+		scheduleSounds(endTime);
 		
+		displayCountdown(endTime);
+		
+	}
+	
+	/**
+	 * Displays the countdown from now to the endTime in a view.
+	 * @param endTime
+	 */
+	private void displayCountdown(long endTime) {
 		long now = Calendar.getInstance().getTimeInMillis();
 		
 		new CountDownTimer(endTime - now + 1000, 1000) {
@@ -101,14 +91,28 @@ public class CountdownActivity extends Activity implements OnTouchListener{
 		     }
 		  }.start();
 	}
-	
-	@Override
-	public boolean onTouch(View v, MotionEvent event) {
-		timer.cancel();
-		finish();
-		return true;
+
+	/**
+	 * Returns a calendar pointing to the closest time having the given minute as minute
+	 * Example: it's 14:22, input is 7
+	 * The output calendar is set to 15:07 today
+	 * @param minute
+	 * @return
+	 */
+	private Calendar getNextMinuteOccurrence(int minute){
+		Calendar out = Calendar.getInstance();
+		if(out.get(Calendar.MINUTE) >= minute)
+			out.add(Calendar.HOUR, 1);
+		out.set(Calendar.SECOND, 0);
+		out.set(Calendar.MINUTE, minute);
+		return out;
 	}
 	
+	/**
+	 * @author tourn
+	 * A TimerTask to play a sound from a SoundPlayer.
+	 *
+	 */
 	private class SoundTask extends TimerTask{
 		Sound sound;
 		
@@ -120,5 +124,29 @@ public class CountdownActivity extends Activity implements OnTouchListener{
 		public void run() {
 			player.play(sound);
 		}
+	}
+	
+	/**
+	 * Schedules sounds of the default voiceset
+	 * @param endTime
+	 */
+	private void scheduleSounds(long endTime){
+		timer = new Timer();
+		player = new Player(Setup.BASE_VOICESET_DIR,Setup.DEFAULT_VOICESET_DIR);
+		
+		//Schedule sounds
+		for (Sound sound : player.getSounds()) {
+//			long schedule = endTime - 1000 * sound.getSecToEnd();
+			long schedule = endTime - 1000 * sound.getSecToEnd() - 1000;
+			if(schedule >= Calendar.getInstance().getTimeInMillis())
+				timer.schedule(new SoundTask(sound), new Date(schedule));
+		}
+		
+		timer.schedule(new TimerTask(){
+			@Override
+			public void run() {
+				finish();
+			}
+		}, new Date(endTime));
 	}
 }
